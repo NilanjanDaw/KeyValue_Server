e0e2e4a8d2b5c7d73d3d61879e2986e3e5a0636f
nilanjandaw <nilanjandaw@gmail.com>
2018-10-03 19:11:58 +0530

    cleaned code

--- server_multithreaded.cpp
+++ server_multithreaded.cpp
@@ -4,7 +4,7 @@
  * @Email:  nilanjandaw@gmail.com
  * @Filename: server.c
  * @Last modified by:   nilanjan
- * @Last modified time: 2018-10-03T18:57:10+05:30
+ * @Last modified time: 2018-10-03T19:11:31+05:30
  * @Copyright: Nilanjan Daw
  */
 
@@ -25,7 +25,7 @@
 #include <limits.h>
 
 #define SLEEP_NANOSEC 100
-#define BUFFER_LENGTH 1024
+#define THREAD_COUNT 8
 #define BACKLOG_QUEUE 5
 #define MAX_NUM_TOKENS 4
 #define QUEUE_SIZE 1024
@@ -50,15 +50,20 @@ void error_handler(const char *error_msg) {
 }
 
 void signal_handler(int signal) {
+
   if (signal == SIGINT || signal == SIGTERM) {
+
     for (int i = 0; i < QUEUE_SIZE; i++) {
+
       if (client_file_descriptor[i] > 0) {
+
         close(client_file_descriptor[i]);
         client_file_descriptor[i] = 0;
         printf("Closed active client connection\n");
       }
     }
     if (socket_file_descriptor > 0) {
+
       printf("Closed server socket\n");
       close(socket_file_descriptor);
     }
@@ -67,6 +72,7 @@ void signal_handler(int signal) {
 }
 
 int send_header(int length, int client_connection) {
+
   char header[11];
   int current_read = 0;
   sprintf(header, "%10d", length);
@@ -77,6 +83,7 @@ int send_header(int length, int client_connection) {
 }
 
 void write_client(int client_connection, const char *buffer) {
+
   send_header(strlen(buffer), client_connection);
   if (write(client_connection, buffer, strlen(buffer)) < 0)
     error_handler("unable to write to socket");
@@ -84,6 +91,7 @@ void write_client(int client_connection, const char *buffer) {
 }
 
 int insert(int new_client) {
+
   client_file_descriptor[insert_index] = new_client;
   insert_index = (++insert_index) % QUEUE_SIZE;
   current_queue_element_count++;
@@ -100,11 +108,14 @@ int retrieve(int *memory) {
 }
 
 int create(int key, char *value) {
+
   if (hashtable[key] != NULL)
     return -1;
+
   char *new_value = (char *) malloc(strlen(value) * sizeof(char));
   bzero(new_value, strlen(value));
   strcpy(new_value, value);
+
   hashtable[key] = new_value;
   return 0;
 }
@@ -114,6 +125,7 @@ char* read(int key) {
 }
 
 int delete_key(int key) {
+
   if (hashtable[key] == NULL)
     return -1;
   hashtable.erase(key);
@@ -121,12 +133,15 @@ int delete_key(int key) {
 }
 
 int update(int key, char *buffer) {
+
   if (hashtable[key] == NULL)
     return -1;
+
   free(hashtable[key]);
   char *new_value = (char *) malloc(strlen(buffer) * sizeof(char));
   bzero(new_value, strlen(buffer));
   strcpy(new_value, buffer);
+
   hashtable[key] = new_value;
   return 0;
 }
@@ -164,6 +179,7 @@ char **tokenize(char *line, long int buffer_length) {
 }
 
 char** read_client(int client_connection, int *n) {
+
   *n = 0;
   int current_read = 0;
   char *buffer = NULL;
@@ -174,6 +190,7 @@ char** read_client(int client_connection, int *n) {
   } else if (current_read == 0) {
     return NULL;
   }
+
   int packet_length = atoi(header);
   printf("%d\n", packet_length);
   buffer = (char *) malloc(packet_length * sizeof(char));
@@ -185,16 +202,19 @@ char** read_client(int client_connection, int *n) {
   char **token = tokenize(buffer, strlen(buffer));
   if (buffer != NULL)
     free(buffer);
+
   return token;
 }
 
 void free_token(char **token) {
+
   for (size_t i = 0; i < 4; i++) {
     if (token[i] != NULL) {
       free(token[i]);
       token[i] = NULL;
     }
   }
+
   if (token != NULL) {
     free(token);
     token = NULL;
@@ -207,10 +227,14 @@ int handle_request(int client_connection) {
   while (1) {
 
     char **token = read_client(client_connection, &n);
+
     if (token != NULL)
       printf("%s|%s|%s|%s\n", token[0], token[1], token[2], token[3]);
+
     if (token == NULL || (token[0], "exit00") == 0) {
+
       if (token != NULL) {
+
         for(size_t i = 0; i < MAX_NUM_TOKENS; i++) {
           if (token[i] != NULL)
             free(token[i]);
@@ -221,13 +245,15 @@ int handle_request(int client_connection) {
       break;
 
     } else if (strcmp(token[0], "create") == 0) {
-      printf("create\n");
+
       long int buffer_len = strtol(token[2], NULL, 10);
       int key = atoi(token[1]);
       if (hashtable[key] == NULL || strlen(hashtable[key]) == 0) {
+
         create(key, token[3]);
         printf("created entry with length %ld\n", strlen(token[3]));
         write_client(client_connection, "Ok\n");
+
       } else {
         printf("Error entry exists\n");
         write_client(client_connection, "Error entry exists\n");
@@ -235,14 +261,14 @@ int handle_request(int client_connection) {
     } else if (strcmp(token[0], "read") == 0) {
 
       int key = atoi(token[1]);
-      printf("Key to find %d\n", key);
       if (hashtable[key] == NULL || strlen(hashtable[key]) == 0) {
         printf("No entry\n");
         write_client(client_connection, "Error no such entry\n");
       }
       else {
+
         char* value = read(key);
-        printf("Key found %s\n", value);
+        printf("Key / value found %d %s\n", key, value);
         write_client(client_connection, value);
       }
 
@@ -254,6 +280,7 @@ int handle_request(int client_connection) {
         write_client(client_connection, "Error no such entry\n");
       }
       else {
+
         delete_key(key);
         printf("Ok\n");
         write_client(client_connection, "Ok\n");
@@ -292,22 +319,27 @@ void *master(void *data) {
   address.sin_family = AF_INET;
   address.sin_addr.s_addr = INADDR_ANY;
   address.sin_port = htons(port);
+
   if (bind(socket_file_descriptor, (struct sockaddr *) &address, sizeof(address)) < 0)
     error_handler("unable to bind to port");
+
   printf("Server socket bound to port %d\n", port);
   if (listen(socket_file_descriptor, BACKLOG_QUEUE) < 0)
     error_handler("unable to listen to given port");
   printf("Server started listening on port %d\n", port);
 
   while(1) {
+
     printf("Waiting for client connections\n");
     socklen_t cli_addr_size = sizeof(client_address);
     int new_connection = accept(socket_file_descriptor, (struct sockaddr *) &client_address,
                                     &cli_addr_size);
+
     if (new_connection < 0)
       error_handler("unable to accept client connection");
     printf("New client connection\n");
     pthread_mutex_lock(&mutex);
+
     while (current_queue_element_count == QUEUE_SIZE) {
       printf("master waiting - connection buffer full\n" );
       pthread_cond_wait(&generator, &mutex);
@@ -321,9 +353,12 @@ void *master(void *data) {
 
 // read request from queue
 void* service_request(void* id) {
+
   int thread_id = *((int *)id);
   while (1) {
+
     pthread_mutex_lock(&mutex);
+
     while (current_queue_element_count <= 0) {
       printf("Buffer empty. Thread %d sleeping.\n", thread_id);
       pthread_cond_wait(&retriever, &mutex);
@@ -341,22 +376,20 @@ void* service_request(void* id) {
 int main(int argc, char *argv[]) {
 
   int prod_thread_id = 0, worker_thread_count;
-  printf("This system has %d processors configured and "
-        "%d processors available.\n",
-        get_nprocs_conf(), get_nprocs());
-  worker_thread_count = 2 * get_nprocs();
   pthread_t prod_thread;
   pthread_t *consumer_threads;
 
   signal(SIGINT, signal_handler);
   signal(SIGTERM, signal_handler);
+
   if (argc < 2)
     error_handler("Port number must be provided");
   port = atoi(argv[1]);
   //create master thread
   pthread_create(&prod_thread, NULL, master, (void *)&prod_thread_id);
   consumer_threads = (pthread_t*) malloc(worker_thread_count * sizeof(pthread_t));
-  for (int i = 1; i <= worker_thread_count; i++) {
+
+  for (int i = 1; i <= THREAD_COUNT; i++) {
     printf("Thread #%d spawned\n", i);
     int id = i;
     pthread_create(&consumer_threads[i - 1], NULL, service_request, (void *)&id);
